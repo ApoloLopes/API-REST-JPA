cd12bc32aeeca6166eabfe38de4c1b4597bdc32e
Apolo Lopes <apoloaugusto10@hotmail.com>
2021-01-25 14:08:47 -0300

    iniciando projeto api rest mvc jpa, utilizando spring initialzr

---
+++ src/main/java/br/com/apolo/apirev/ApiRevApplication.java
@@ -0,0 +1,13 @@
+package br.com.apolo.apirev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/apolo/apirev/ApiRevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apolo.apirev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
