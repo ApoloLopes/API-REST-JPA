5114d9dbc8614668f5c7e88589fc45c4371fb598
Apolo Lopes <apoloaugusto10@hotmail.com>
2021-01-26 14:57:42 -0300

    terminando a classe person com idexes para não repetir nome e sobrenome

--- src/main/java/br/com/apolo/apirev/entity/Person.java
+++ src/main/java/br/com/apolo/apirev/entity/Person.java
@@ -1,4 +1,84 @@
 package br.com.apolo.apirev.entity;
 
-public class Person {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "PERSONS",
+
+    //uniqueConstraints = {@UniqueConstraint(columnNames="FIRST_NAME,LAST_NAME", name = "IDX_1")}
+    indexes = {@Index(columnList = "FIRST_NAME,LAST_NAME", name = "IDX_PERSON_NAME", unique = true)}
+
+    )
+public class Person implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_PERSON")
+    private Long id;
+
+    @Column(name = "FIRST_NAME", nullable = false, length = 30)
+    private String firstName;
+
+    @Column(name = "LAST_NAME", nullable = false, length = 60)
+    private String lastName;
+
+    @Column(name = "AGE", nullable = false)
+    private Integer age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id.equals(person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
